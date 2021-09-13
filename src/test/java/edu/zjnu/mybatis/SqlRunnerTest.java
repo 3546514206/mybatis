4b9a54773088cebf3bab0d615749ca6f6dce5059
杨海波 <3546514206@qq.com>
2021-09-13 17:38:48 +0800

    SqlRunner 插入

--- src/test/java/edu/zjnu/mybatis/SqlRunnerTest.java
+++ src/test/java/edu/zjnu/mybatis/SqlRunnerTest.java
@@ -3,6 +3,7 @@ package edu.zjnu.mybatis;
 import com.alibaba.fastjson.JSON;
 import edu.zjnu.JsonPrintUtil;
 import jdk.nashorn.internal.runtime.JSONFunctions;
+import org.apache.ibatis.jdbc.SQL;
 import org.apache.ibatis.jdbc.SqlRunner;
 import org.junit.Test;
 
@@ -30,8 +31,17 @@ public class SqlRunnerTest {
 
         SqlRunner sqlRunner = new SqlRunner(connection);
 
+        // 查询
         Map<String, Object> map = sqlRunner.selectOne("select t.* from user t where id = ?", 2);
 
         System.out.println(JsonPrintUtil.formatToJson(JSON.toJSONString(map)));
+
+        // 插入数据
+        SQL insertSql = new SQL().INSERT_INTO("USER ").VALUES("PASSWORD","?").VALUES("USERNAME","?");
+        System.out.println(insertSql);
+
+        sqlRunner.insert(insertSql.toString(),"123","jetty");
+        sqlRunner.insert(insertSql.toString(),"321","lose");
+        sqlRunner.insert(insertSql.toString(),"111","rose");
     }
 }
