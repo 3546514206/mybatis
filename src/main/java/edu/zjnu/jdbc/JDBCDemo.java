d4705b6bd0f38bbf5f3cec06213508c77752fe68
杨海波 <3546514206@qq.com>
2021-09-13 11:16:44 +0800

    更新注释

--- src/main/java/edu/zjnu/jdbc/JDBCDemo.java
+++ src/main/java/edu/zjnu/jdbc/JDBCDemo.java
@@ -3,7 +3,7 @@ package edu.zjnu.jdbc;
 import java.sql.*;
 
 /**
- * @description: todo
+ * @description: JDBCDemo
  * @author: 杨海波
  * @date: 2021-09-09
  **/
@@ -14,18 +14,23 @@ public class JDBCDemo {
         Connection conn = null;
         Statement statement = null;
         ResultSet resultSet = null;
+
         try {
             //1.加载驱动
             // DriverManager.registerDriver(new Driver());
             Class.forName("com.mysql.jdbc.Driver");//加载类时就会加载驱动,需要导入jar包
+
             //2.获得链接
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis", "root", "Yhb199605"); //替换成自己的数据库名称
+
             //3.创建执行SQL语句的对象，执行SQl
             //3.1 创建执行SQL的对象
             String str = "SELECT * FROM user";
             statement = conn.createStatement();
+
             //3.2 执行SQL语句
             resultSet = statement.executeQuery(str);
+
             while (resultSet.next()) {
                 int uid = resultSet.getInt("id");
                 String username = resultSet.getString("username");
@@ -37,6 +42,7 @@ public class JDBCDemo {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+
             //4.释放资源
             if (conn != null) {
                 try {
