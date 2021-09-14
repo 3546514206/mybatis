c2722c721d7fd4963669ce92fa1b0cf4a4204f36
杨海波 <3546514206@qq.com>
2021-09-14 23:09:19 +0800

    StatementHandler注释

--- src/main/java/org/apache/ibatis/executor/statement/StatementHandler.java
+++ src/main/java/org/apache/ibatis/executor/statement/StatementHandler.java
@@ -15,26 +15,41 @@
  */
 package org.apache.ibatis.executor.statement;
 
+import org.apache.ibatis.executor.parameter.ParameterHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.session.ResultHandler;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
-import org.apache.ibatis.executor.parameter.ParameterHandler;
-import org.apache.ibatis.mapping.BoundSql;
-import org.apache.ibatis.session.ResultHandler;
-
 /**
  * StatementHandler封装了对JDBC Statement对象的操作，
- * 比如为Statement对象设置参数，调用Statement接口提供的方法与数据库交互，等等。
+ * 比如为Statement对象设置参数，调用Statement接口提供的方法与数据库交互，
+ * 设置Statement对象的fetchSize属性、设置查询超时时间、调用JDBC Statement与数据库交互等。
  *
  * @author Clinton Begin
  */
 public interface StatementHandler {
 
+    /**
+     * 该方法用于创建JDBC Statement对象，并完成Statement对象的属性设置。
+     *
+     * @param connection
+     * @return
+     * @throws SQLException
+     */
     Statement prepare(Connection connection)
             throws SQLException;
 
+    /**
+     * 该方法使用MyBatis中的ParameterHandler组件为
+     * PreparedStatement和CallableStatement参数占位符设置值。
+     *
+     * @param statement
+     * @throws SQLException
+     */
     void parameterize(Statement statement)
             throws SQLException;
 
